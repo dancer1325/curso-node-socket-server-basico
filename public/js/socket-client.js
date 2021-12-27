a39bbc8ae5421340e07041dd35f9f74aa01ae7bf
dancer1325 <alfredotic0809@gmail.com>
2021-12-27 13:36:01 +0100

    Add commentaries -- Section 14 --

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,4 +1,4 @@
-
+// File to custom the events via WebSocket between the client and the server
 // Referencias del HTML
 const lblOnline  = document.querySelector('#lblOnline');
 const lblOffline = document.querySelector('#lblOffline');
@@ -6,42 +6,42 @@ const txtMensaje = document.querySelector('#txtMensaje');
 const btnEnviar  = document.querySelector('#btnEnviar');
 
 
-const socket = io();
+const socket = io(); // Comes from /socket.io/socket.io library
 
 
 
-socket.on('connect', () => {
+socket.on('connect', () => { // Listener to the "connect" event. Launched in case the socket client connects to the server
     // console.log('Conectado');
 
-    lblOffline.style.display = 'none';
-    lblOnline.style.display  = '';
+    lblOffline.style.display = 'none'; // Not to show this html element
+    lblOnline.style.display  = ''; // Show this html element
 
 });
 
-socket.on('disconnect', () => {
+socket.on('disconnect', () => { // Launched in case the socket server is down
     // console.log('Desconectado del servidor');
 
-    lblOnline.style.display  = 'none';
-    lblOffline.style.display = '';
+    lblOnline.style.display  = 'none'; // Not to show this html element
+    lblOffline.style.display = ''; // Show this html element
 });
 
 
-socket.on('enviar-mensaje', (payload) => {
+socket.on('enviar-mensaje', (payload) => { // Launched in case the socket server emit the event "enviar-mensaje"
     console.log( payload )
 })
 
 
-btnEnviar.addEventListener( 'click', () => {
+btnEnviar.addEventListener( 'click', () => { // Add event listener to the "btnEnviar" HTML element under action "click"
 
-    const mensaje = txtMensaje.value;
+    const mensaje = txtMensaje.value; // Get the value of the "txtMensaje" HTML element
     const payload = {
         mensaje,
         id: '123ABC',
         fecha: new Date().getTime()
     }
     
-    socket.emit( 'enviar-mensaje', payload, ( id ) => {
-        console.log('Desde el server', id );
+    socket.emit( 'enviar-mensaje', payload, ( id ) => { // Emit events
+        console.log('Desde el server', id ); // Add callback function to check in the client side if the socket connection was fine
     });
 
 });
--- sockets/controller.js
+++ sockets/controller.js
@@ -1,20 +1,23 @@
 
 
 
-const socketController = (socket) => {
+const socketController = (socket) => { // Socket object represents just the client (not for all) connected to the server which triggers the call to the server
     
-    console.log('Cliente conectado', socket.id );
+    console.log('Cliente conectado', socket.id ); // This log appears each time a connection with some client is established. socket.id : Socket's id
 
-    socket.on('disconnect', () => {
+    socket.on('disconnect', () => { // Socket to listen on 'disconnect' event. This event is launched each time a client disconnects . Example : Refreshing a webpage
         console.log('Cliente desconectado', socket.id );
     });
 
-    socket.on('enviar-mensaje', ( payload, callback ) => {
+    socket.on('enviar-mensaje', ( payload, callback ) => { // Socket to listen on 'enviar-mensaje' event
         
         const id = 123456789;
-        callback( id );
+        callback( id ); // Execute callback function to check in the client side if the socket connection was fine.
+        // It will be executed just for the client which launched the event, not for all
+        console.log(payload)
 
-        socket.broadcast.emit('enviar-mensaje', payload );
+        //socket.emit('enviar-mensaje', payload ); // It will launch the event and it will be listened just by the client which triggers the call to the server
+        socket.broadcast.emit('enviar-mensaje', payload ); // To be launched to all clients connected to the server via socket, not just the one which trigger the call to the server
 
     })
 
